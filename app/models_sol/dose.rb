1a8a8ef0f9a50be0c73cc9e68e05efdc82a1fda8
Emmanuel MARIE <emmanuel.marie@qlapoti.fr>
2020-05-15 15:42:29 +0200

    test1

--- app/models_sol/dose.rb
+++
@@ -1,5 +0,0 @@
-class Dose < ApplicationRecord
-  belongs_to :cocktail
-  belongs_to :ingredient
-  validates :cocktail, uniqueness: { scope: :ingredient }
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "cocktails#index"
+  # For details on the DSL available within this file, see https://guides.rubyo<nrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:create]
+    resources :doses, only: [:new, :create]
     resources :reviews, only: [:create]
   end
   resources :doses, only: [:destroy]
 end
+
